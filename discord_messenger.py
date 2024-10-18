520bfad9b4a72e8c49cd97fdf2ca01320696fe70
Leo Hung <srleohung@gmail.com>
2024-10-18 13:03:50 +0800

    Update discord messenger to pass delete_after

--- discord_messenger.py
+++ discord_messenger.py
@@ -28,7 +28,7 @@ class DiscordMessenger:
         self.client = DiscordClient(intents=discord.Intents.default())
         self.loop = asyncio.get_event_loop()
 
-    def send(self, message):
+    def send(self, message, delete_after=None):
         """
         Sends a message to the specified Discord channel asynchronously.
 
@@ -36,9 +36,9 @@ class DiscordMessenger:
             message (str): The message content to send to the Discord channel.
         """
         # Schedule the coroutine to run in the client's event loop
-        asyncio.run_coroutine_threadsafe(self.client.send_message(self.channel_id, message), self.client.loop)
+        asyncio.run_coroutine_threadsafe(self.client.send_message(self.channel_id, message, delete_after=delete_after), self.client.loop)
     
-    def send_image(self, image_bytes, filename="image.png"):
+    def send_image(self, image_bytes, filename="image.png", delete_after=None):
         """
         Sends an image to the specified Discord channel asynchronously.
 
@@ -46,9 +46,9 @@ class DiscordMessenger:
             image_bytes (bytes): The image data as bytes.
             filename (str): The filename to use when sending the image (default: "image.png").
         """
-        asyncio.run_coroutine_threadsafe(self.client.send_image(self.channel_id, image_bytes, filename), self.client.loop)
+        asyncio.run_coroutine_threadsafe(self.client.send_image(self.channel_id, image_bytes, filename, delete_after=delete_after), self.client.loop)
 
-    def send_screencap(self, screencap, top_left, bottom_right, filename="screencap.png"):
+    def send_screencap(self, screencap, top_left, bottom_right, filename="screencap.png", delete_after=None):
         """
         Converts a numpy array screencap to bytes and sends it to the Discord channel.
 
@@ -75,7 +75,7 @@ class DiscordMessenger:
         image_bytes_io.seek(0)  # Go to the start of the BytesIO buffer
 
         # Send the image to the Discord channel
-        asyncio.run_coroutine_threadsafe(self.client.send_image(self.channel_id, image_bytes_io.getvalue(), filename), self.client.loop)
+        asyncio.run_coroutine_threadsafe(self.client.send_image(self.channel_id, image_bytes_io.getvalue(), filename, delete_after=delete_after), self.client.loop)
 
     def start(self):
         """
@@ -117,7 +117,7 @@ class DiscordClient(discord.Client):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-    async def send_message(self, channel_id, message):
+    async def send_message(self, channel_id, message, delete_after=None):
         """
         Sends a message to a specified channel after ensuring the bot is ready.
 
@@ -128,11 +128,11 @@ class DiscordClient(discord.Client):
         await self.wait_until_ready()  # Wait until the bot is fully connected and ready
         channel = self.get_channel(channel_id)  # Get the channel by ID
         if channel:
-            await channel.send(message)  # Send the message if the channel is found
+            await channel.send(message, delete_after=delete_after)  # Send the message if the channel is found
         else:
             print(f"Channel ID {channel_id} not found.")
     
-    async def send_image(self, channel_id, image_bytes, filename):
+    async def send_image(self, channel_id, image_bytes, filename, delete_after=None):
         """
         Sends an image as bytes to a specified channel after ensuring the bot is ready.
 
@@ -145,6 +145,6 @@ class DiscordClient(discord.Client):
         channel = self.get_channel(channel_id)  # Get the channel by ID
         if channel:
             image_file = discord.File(BytesIO(image_bytes), filename=filename)
-            await channel.send(file=image_file)  # Send the image file to the channel
+            await channel.send(file=image_file, delete_after=delete_after)  # Send the image file to the channel
         else:
             print(f"Channel ID {channel_id} not found.")
